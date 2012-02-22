643a7bd49b10255268d43af84b5831d354023ba3
Michael Ibarra <AMER\mibarra@gettyimages.com>
2012-02-21 16:43:02 -0800

    Added some assertions. Ignored files.

---
+++ Sahara.UnitTests/Making_assetions.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+
+namespace Sahara.UnitTests
+{
+    [TestFixture]
+    public class Making_assetions
+    {
+        [Test]
+        public void Assertions_should_check_values()
+        {
+            (new object() as string).ShouldBeNull();
+            "Monkey".ShouldNotBeNull();
+            "Monkey".ShouldEqual("Monkey");
+            "Monkey".ShouldNotEqual("Ape");
+            3.ShouldBeGreaterThan(2);
+            3.ShouldBeAtLeast(2);
+            2.ShouldBeLessThan(3);
+            2.ShouldBeAtMost(3);
+            3.ShouldEqual(3);
+            3.ShouldBeAtLeast(3);
+            3.ShouldBeAtMost(3);
+
+        }
+        
+    }
+}
---
+++ Sahara/TestExtensions.cs
@@ -0,0 +1,56 @@
+using System;
+using NUnit.Framework;
+
+namespace Sahara
+{
+    public static class TestExtensions
+    {
+        public static T ShouldBeNull<T>(this T actual)
+        {
+            Assert.IsNull(actual);
+            return actual;
+        }
+
+        public static T ShouldNotBeNull<T>(this T actual)
+        {
+            Assert.IsNotNull(actual);
+            return actual;
+        }
+
+        public static T ShouldEqual<T>(this T actual, T expected)
+        {
+            Assert.AreEqual(expected, actual);
+            return actual;
+        }
+
+        public static T ShouldNotEqual<T>(this T actual, T expected)
+        {
+            Assert.AreNotEqual(expected, actual);
+            return actual;
+        }
+
+        public static IComparable ShouldBeGreaterThan(this IComparable actual, IComparable expected)
+        {
+            Assert.Greater(actual, expected);
+            return actual;
+        }
+
+        public static IComparable ShouldBeAtLeast(this IComparable actual, IComparable expected)
+        {
+            Assert.GreaterOrEqual(actual, expected);
+            return actual;
+        }
+
+        public static IComparable ShouldBeLessThan(this IComparable actual, IComparable expected)
+        {
+            Assert.Less(actual, expected);
+            return actual;
+        }
+
+        public static IComparable ShouldBeAtMost(this IComparable actual, IComparable expected)
+        {
+            Assert.LessOrEqual(actual, expected);
+            return actual;
+        }
+    }
+}
