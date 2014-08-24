8537ab59cdf41036efe5344fb9abb96c8ab8e3cd
Michael Ibarra <michael.ibarra@avalara.com>
2014-08-24 13:12:39 -0700

    Updated to 1.3.0. Added auto-mocking framwork & some new assertions.

---
+++ nuget_build.ps1
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe .\Sahara\Sahara.csproj /p:Configuration="Release"
+.\.nuget\NuGet.exe pack .\Sahara\Sahara.csproj -Prop Configuration=Release
